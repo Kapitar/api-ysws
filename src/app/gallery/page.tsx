6cbb4602bbed2d8f710b0a35e9019486039ee746
Kapitar <kkapitar@ya.ru>
2025-07-26 22:17:31 +0900

    feat: add api to the gallery and better design

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,26 +1,67 @@
+"use client";
+
+import Loader from "@/components/Loader";
 import ProjectCard from "@/components/ProjectCard";
+import { useEffect, useState } from "react";
+
+interface Submission {
+  id: string;
+  fields: {
+    "Project Name": string;
+    "Code URL": string;
+    "Description": string;
+    "Playable URL": string;
+  };
+}
 
 export default function Gallery() {
+  const [isLoading, setLoading] = useState(true);
+  const [submissions, setSubmissions] = useState<Submission[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api2.hackclub.com/v0.1/Hackmate/Hackmate Project Submission");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setSubmissions(data);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-2 mt-52 gap-x-4">
-        <ProjectCard
-          id="123132131"
-          name="Project name"
-          githubLink="https://github.com/"
-          swaggerLink="https://editor.swagger.io/?_gl=1*dxaxx2*_gcl_au*Mzc2NzkyOTEuMTc1MzUzMDc1MA.."
-          description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          createdAt={new Date()}
-        />
-
-        <ProjectCard
-          id="123132131"
-          name="Project name"
-          githubLink="https://github.com/"
-          swaggerLink="https://editor.swagger.io/?_gl=1*dxaxx2*_gcl_au*Mzc2NzkyOTEuMTc1MzUzMDc1MA.."
-          description="lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          createdAt={new Date()}
-        />
+      <div className="p-4 border-2 border-gray-700 rounded-2xl mt-10">
+        <p>
+          <span className="text-green-600 font-bold">orpheus@hackclub:</span>
+          <span className="text-blue-600 font-bold">~</span>
+          <span className="text-white font-bold">$ </span>
+          <span className="text-blue-600">curl </span>
+          https://endpointer.hackclub.com/api/gallery
+        </p>
+      </div>
+      <div className="grid grid-cols-2 mt-8 gap-4 items-stretch">
+        {isLoading ? <Loader /> : submissions.map((submission) => {
+          return (
+            <ProjectCard
+              key={submission.id}
+              id={submission.id}
+              name={submission.fields['Project Name']}
+              githubLink={submission.fields['Code URL']}
+              swaggerLink={submission.fields['Playable URL']}
+              description={submission.fields['Description'].trim()}
+              createdAt={new Date()}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -29,17 +29,17 @@ export default function ProjectCard({
   };
 
   return (
-    <div className="w-full">
-      <div className="border-2 border-gray-700 rounded-xl px-4 py-4">
+    <div className="w-full h-full">
+      <div className="border-2 border-gray-700 rounded-xl px-4 py-4 h-full flex flex-col gap-4">
         <JSONPlaceholder data={data} />
 
-        <div className="flex gap-x-3">
-          <a href={swaggerLink} className="py-2.5 px-4 bg-blue-700 rounded-lg mt-4 hover:bg-blue-800">
+        <div className="mt-auto flex gap-x-3">
+          <a href={swaggerLink} className="py-2.5 px-4 bg-blue-700 rounded-lg hover:bg-blue-800">
             <RxOpenInNewWindow className="inline-block mr-2" />
             Try it out!
           </a>
 
-          <a href={githubLink} className="py-2.5 px-4 bg-gray-700 rounded-lg mt-4 hover:bg-gray-800">
+          <a href={githubLink} className="py-2.5 px-4 bg-gray-700 rounded-lg hover:bg-gray-800">
             <FaGithub className="inline-block mr-2" />
             Github
           </a>
