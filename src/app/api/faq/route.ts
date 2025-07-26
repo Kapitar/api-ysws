babedba8ffca2f407b1c58bbaa8897ca01309a12
Kapitar <kkapitar@ya.ru>
2025-07-26 21:31:31 +0900

    feat: create faq route

---
+++ src/app/api/faq/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  return NextResponse.json(
+    { 
+      faq: {
+        "What languages can I use?": "You can use any languages to create your API",
+        "What can I buy for this grant?": "You can buy anything that helps you build your API, such as a Raspberry Pi or cloud credits",
+        "Where can I ask for help?": "You can ask for help in the #endpointer channel on the Hack Club Slack",
+      },
+    }
+  );
+}
