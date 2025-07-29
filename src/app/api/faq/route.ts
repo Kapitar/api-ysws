892d34c11d6723d195f927ac1f6cedc3983b265c
Kapitar <kkapitar@ya.ru>
2025-07-29 10:09:08 -0700

    feat: add a requirement and faq

--- src/app/api/faq/route.ts
+++ src/app/api/faq/route.ts
@@ -8,6 +8,7 @@ export async function GET(request: Request) {
         "What can I buy for this grant?": "You can buy anything that helps you build your API, such as a Raspberry Pi or Cloud Credits",
         "Where can I ask for help?": "You can ask for help in the #endpointer channel on the Hack Club Slack",
         "When does it end?": "The Endpointer program ends on August 12, 23:59 EST",
+        "Can I double dip with SOM": "Yes, you can double dip with the Summer of Making program"
       },
     }
   );
--- src/app/api/requirements/route.ts
+++ src/app/api/requirements/route.ts
@@ -8,6 +8,7 @@ export async function GET(request: Request) {
       "Your API must have at least three GET and three POST endpoints.",
       "Your API must have at least one PUT and one DELETE endpoint.",
       "Your API must have a database to store data.",
+      "AI is allowed in a reasonable amount; fully vibe-coded projects will be rejected. Documentation and README must be written by you.",
       "Document your entire API using Swagger (OpenAPI).",
       "Log at least five hours of work using Hackatime and get $25 grant.",
       "If you add JWT authentication and create a protected path for authenticated users and log at least 8 hours, you will get $40 grant",
