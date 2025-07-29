88c66ff2103bdd5ba28c04a3673ca20f79a95987
Kapitar <kkapitar@ya.ru>
2025-07-28 21:52:05 -0700

    fix: typos in faq

--- src/app/api/faq/route.ts
+++ src/app/api/faq/route.ts
@@ -4,8 +4,8 @@ export async function GET(request: Request) {
   return NextResponse.json(
     { 
       faq: {
-        "What languages can I use?": "You can use any languages to create your API",
-        "What can I buy for this grant?": "You can buy anything that helps you build your API, such as a Raspberry Pi or cloud credits",
+        "What languages can I use?": "You can use any language to create your API",
+        "What can I buy for this grant?": "You can buy anything that helps you build your API, such as a Raspberry Pi or Cloud Credits",
         "Where can I ask for help?": "You can ask for help in the #endpointer channel on the Hack Club Slack",
         "When does it end?": "The Endpointer program ends on August 12, 23:59 EST",
       },
